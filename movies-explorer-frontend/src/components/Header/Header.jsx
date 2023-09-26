2663edc04642d883fb5e077d2c58dcbcd2ce2c94
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-09-26 21:46:58 +0600

    fonts and header

---
+++ movies-explorer-frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import logo from "../../images/logo.svg";
+import account from "../../images/logo-account.svg";
+
+export default function Header({}) {
+  return (
+    <header className="header">
+      <img src={logo} className="logo" alt="логотип" />
+      <div className="header__container-films">
+        <div className="header__container-films">
+          <p className="header__films">Фильмы</p>
+          <p className="header__films">Сохранённые фильмы</p>
+        </div>
+        <div className="header__container-account">
+          <p className="header__account">Аккаунт</p>
+          <img  className="header__logo-account" src={account} alt="логотип аккаунта" />
+        </div>
+      </div>
+    </header>
+  );
+}
