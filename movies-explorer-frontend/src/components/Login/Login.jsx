6b7ec8bef60fa083f8f0685241103de8de236184
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-10-02 15:45:03 +0600

    added full login

--- movies-explorer-frontend/src/components/Login/Login.jsx
+++ movies-explorer-frontend/src/components/Login/Login.jsx
@@ -1,37 +1,46 @@
+import { Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
 
 export default function Login({}) {
   return (
     <div className="login">
-      <img src={logo} className="login__logo" alt="логотип сайта" />
-      <h1 className="login__title">Рады видеть!</h1>
+      <div className="login__flex-container">
+        <img src={logo} className="login__logo" alt="логотип сайта" />
+        <h1 className="login__title">Рады видеть!</h1>
+      </div>
       <form className="login__form">
+        <p className="login__input-text">E-mail</p>
         <input
           type="text"
           name="email"
-          placeholder="E-mail"
+          placeholder="email@mail.ru"
           className="login__input login__input_type_email"
           minLength={2}
           maxLength={40}
           required=""
-        //   value={values.email || ""}
-        //   onChange={handleChange}
+          //   value={values.email || ""}
+          //   onChange={handleChange}
         />
+        <p className="login__input-text">Пароль</p>
         <input
           type="password"
           name="password"
           placeholder="Пароль"
-          className="signIn__input signIn__input_type_password"
+          className="login__input login__input_type_password"
           minLength={2}
           maxLength={400}
           required=""
-        //   value={values.password || ""}
-        //   onChange={handleChange}
+          //   value={values.password || ""}
+          //   onChange={handleChange}
         />
-        <button className="login__button">
-          Войти
-        </button>
+        <button className="login__button">Войти</button>
       </form>
+      <div className="login__container">
+        <p className="login__text">Ещё не зарегистрированы?</p>
+        <Link to="/signup" className="login__link">
+          Регистрация
+        </Link>
+      </div>
     </div>
   );
 }
