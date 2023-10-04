6cb82abd6e8c6ac6dd03183bb021692d5b7f7df8
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-10-04 20:21:50 +0600

    added preloader

--- movies-explorer-frontend/src/components/Movies/Preloader/Preloader.jsx
+++ movies-explorer-frontend/src/components/Movies/Preloader/Preloader.jsx
@@ -1,6 +1,8 @@
+import preloader from "../../../images/preloader.svg";
+
 export default function Preloader({}) {
     return (
         <div className="preloader">
-            
+          <img src={preloader} className="preloader__image" alt="" />
         </div>
     )}
