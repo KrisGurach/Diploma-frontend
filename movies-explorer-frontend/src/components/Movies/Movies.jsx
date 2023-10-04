3418fcc8c15f8843c4b345daf19ccd4876404ef3
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-10-04 20:21:36 +0600

    added opacity and preloader

--- movies-explorer-frontend/src/components/Movies/Movies.jsx
+++ movies-explorer-frontend/src/components/Movies/Movies.jsx
@@ -1,10 +1,12 @@
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
+import Preloader from "./Preloader/Preloader";
 import SearchForm from "./SearchForm/SearchForm";
 
 export default function Movies({}) {
   return (
     <div className="Movies">
       <SearchForm />
+      <Preloader />
       <MoviesCardList />
       {/* <Preloader /> */}
     </div>
