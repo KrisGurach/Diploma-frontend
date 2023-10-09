da97e5cb4316ff431ae45180a0d79882eadb14b4
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-10-09 10:22:46 +0600

    320

--- movies-explorer-frontend/src/components/Movies/SearchForm/SearchForm.jsx
+++ movies-explorer-frontend/src/components/Movies/SearchForm/SearchForm.jsx
@@ -3,24 +3,30 @@ import icon from "../../../images/search-icon.svg";
 export default function SearchForm({}) {
   return (
     <div className="searchForm">
-        <form className="searchForm__form">
-          <div className="searchForm__container">
-            <img src={icon} className="searchForm__icon" alt="поиск фильма" />
-            <input  
-              type="text"
-              name="Фильм"
-              placeholder="Фильм" 
-              className="searchForm__input searchForm__input_film" 
-            />
+      <form className="searchForm__form">
+        <div className="searchForm__container">
+          <img src={icon} className="searchForm__icon" alt="поиск фильма" />
+          <input
+            type="text"
+            name="film"
+            placeholder="Фильм"
+            className="searchForm__input searchForm__input_film"
+          />
+        </div>
+        <div className="searchForm__container">
+          <div className="seachForm__container-button">
+            <button className="searchForm__search-button">Найти</button>
           </div>
-          <div className="searchForm__container">
-            <div className="seachForm__container-button">
-              <button className="searchForm__search-button">Найти</button>
-            </div>
+          <div className="searchForm__button-container1280">
             <button className="searchForm__button searchForm__button_active"></button>
             <p className="searchForm__checkbox-text">Короткометражки</p>
           </div>
-        </form>
+        </div>
+      </form>
+      <div className="searchForm__button-container320">
+        <button className="searchForm__button searchForm__button_active"></button>
+        <p className="searchForm__checkbox-text">Короткометражки</p>
+      </div>
     </div>
   );
 }
