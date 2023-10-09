71e620b3c0280d038fd95327505aea488dae5c06
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-10-09 10:53:58 +0600

    remove inter and add remove-button

--- movies-explorer-frontend/src/components/Movies/MoviesCard/MoviesCard.jsx
+++ movies-explorer-frontend/src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -1,12 +1,23 @@
+import { useLocation } from "react-router-dom";
 import image from "../../../images/moviecard.svg";
 
 export default function MoviesCard({isSaved = false}) {
-  const saveButtonClass = `movieCard__save-button ${isSaved ? "movieCard__save-button_active" : "movieCard__save-button_disable"}`;
+  const {pathname} = useLocation();
+  const saveButtonClass = `movieCard__save-button 
+  ${pathname === "/saved-movies" 
+      ? "movieCard__remove-button" 
+      : isSaved 
+          ? "movieCard__save-button_active" 
+          : "movieCard__save-button_disable"}`;
+
+  console.log(pathname);
+  console.log(saveButtonClass);     
+  console.log();   
   
     return (
       <div className="movieCard">
         <img src={image} className="movieCard__image" alt="постер фильма" />
-        <button className={saveButtonClass}>{isSaved ?  "" : "Сохранить" }</button>
+        <button className={saveButtonClass}>{isSaved || pathname === "/saved-movies"  ?  "" : "Сохранить" }</button>
         <div className="movieCard__description">
           <p className="movieCard__name">Баския: Взрыв реальности</p>
           <div className="movieCard__duration">
